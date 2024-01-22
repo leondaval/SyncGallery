966a05d670c2b8bee6876f1b2c67c2cc68406516
Marco <leondaval18@gmail.com>
2024-01-22 11:32:01 +0100

    Aggiornata richiesta di permessi (popup)
    In lavorazione...

--- app/src/main/java/app/SyncGallery/MainActivity.java
+++ app/src/main/java/app/SyncGallery/MainActivity.java
@@ -10,7 +10,6 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -304,42 +303,13 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET}, PERMISSION_REQUEST_CODE);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            boolean allPermissionsGranted = true;
-
-            for (int grantResult : grantResults) {
-
-                if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                    allPermissionsGranted = false;
-                    break;
-                }
-
-            }
-
-            if (allPermissionsGranted)
-                Toast.makeText(MainActivity.this, "Puoi utilizzare la rete!", Toast.LENGTH_SHORT).show();
-
-             else
-                Toast.makeText(MainActivity.this, "Permesso di utilizzo rete negato!", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
     private boolean checkPermission() {
-        return Environment.isExternalStorageManager();
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
     }
 
     private void requestPermission() {
-        Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
-        requestPermissionLauncher.launch(intent);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
     }
-
-    ActivityResultLauncher<Intent> requestPermissionLauncher =
-            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-            });
 
     private ActivityResultLauncher<Intent> directoryLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -359,6 +329,21 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permesso concesso,grazie!", Toast.LENGTH_SHORT).show();
+
+             else
+                // Permesso negato, gestisci di conseguenza
+                Toast.makeText(this, "Permesso negato, l'app potrebbe non funzionare correttamente.", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     private void openDirectory() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
